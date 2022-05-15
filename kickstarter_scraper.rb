925fb1a94deee247b515f6cd578c6385dc49ae47
victor castro <victor_castro_01@hotmail.com>
2022-05-15 07:06:06 -0400

    commit tree 433237a1d54740ad5b2e4f6bae160c9592e2f105

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,33 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
+
+# projects: kickstarter.css("li.project.grid_4") => projects
+# project.css("h2.bbcard_name strong a").text  => titles
+# project.css("div.project-thumbnail a img").attribute("src").value  => image
+# project.css("ul.project-meta span.location-name").text  => location
+# project.css("ul.project-stats li.first.funded strong").text => funded
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  #binding.pry
+
+  projects = {}
+
+  #iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+  # Return the projects hash
+  projects
 end
+
+create_project_hash
